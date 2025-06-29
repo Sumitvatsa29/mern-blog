649ecb324f2868a49a24911463866bce7887e784
Sumit Vatsa <sumitvatsa29@gmail.com>
2025-06-29 17:06:07 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv  from "dotenv";
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -12,11 +13,14 @@ mongoose.connect(process.env.MONGOO).then(()=>{
 });
 const app = express();
 
+app.use(express.json()); // this line help us to send json file in the backend
+
 app.listen(3000, () => {
   console.log("server is running on the 3000 Port E");
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth',authRoutes);
 
 app.get('/test',(req,res)=>{
     res.json({message: 'API is working'});
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -21,6 +21,6 @@ const userSchema = new mongoose.Schema({
 },{timestamps: true}
 );
 
-const User = mongoose.model('User', userschema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
